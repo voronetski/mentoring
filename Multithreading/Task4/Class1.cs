0b83dd25f3c6cb749018bf708e5a0b8092feac99
Maxim Voronetski <Maxim_Voronetski@epam.com>
2018-12-10 23:14:17 +0300

    12

--- Multithreading/Task4/Class1.cs
+++ Multithreading/Task4/Class1.cs
@@ -13,36 +13,59 @@ namespace Task4
 {
     public class Task4
     {
+        const int NUM_THREADS = 1;
+        static Thread[] array;
+        static int j;
+
+        static void CreateThread(int n)
+        {
+            if (n > 0)
+            {
+                var num = (NUM_THREADS - n) + 1;
+                Console.WriteLine("Creating thread #" + num);
+                array[num - 1] = new Thread(() => Decrement(ref j));
+                //array[num - 1].Start(j);
+            }
+            n--;
+            CreateThread(n);
+        }
+
+
         static void Main(string[] args)
         {
-
-            var j = 0;
             Int32.TryParse(Console.ReadLine(), out j);
             var stopwatch = Stopwatch.StartNew();
 
-            Thread[] array = new Thread[4];
+            array = new Thread[NUM_THREADS];
+            //for (int i = 0; i < array.Length; i++)
+            //{
+            //    // Start the thread with a ThreadStart.
+            //    array[i] = new Thread(Decrement);
+            //    array[i].Start();
+            //}
+
+            CreateThread(NUM_THREADS);
+
+            return;
             for (int i = 0; i < array.Length; i++)
             {
-                // Start the thread with a ThreadStart.
-                array[i] = new Thread(Decrement);
                 array[i].Start();
             }
 
+
             for (int i = 0; i < array.Length; i++)
             {
+                array[i].Join();
             }
 
             Console.WriteLine("DONE: {0}", stopwatch.ElapsedMilliseconds);
             Console.ReadKey();
         }
 
-        static void Decrement(object state)
+        static void Decrement(ref int state)
         {
-            var value = (int) state;
-
-            value--;
-            state = value;
-
+            state--;
+            Console.WriteLine(state);
         }
     }
 }
